7f98ff6cfa33c661490611c94de7412260b256b0
Your Name <youremail@domain.com>
2023-04-20 12:39:00 +0800

    h

--- src/main/java/com/example/demo2/MainController.java
+++ src/main/java/com/example/demo2/MainController.java
@@ -3,7 +3,6 @@ package com.example.demo2;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -23,12 +22,10 @@ public class MainController {
     }
 
     private void handleGitReset() {
-        // 创建一个对话框，用于接收用户输入的参数
         Dialog<String> dialog = new Dialog<>();
         dialog.setTitle("Git Reset");
         dialog.setHeaderText("请输入Git Reset参数：");
 
-        // 设置一个标签和输入框，并将其添加到对话框中
         Label label = new Label("参数：");
         TextField textField = new TextField();
 
@@ -37,11 +34,9 @@ public class MainController {
         layout.setSpacing(10);
         dialog.getDialogPane().setContent(layout);
 
-        // 添加确定和取消按钮
         ButtonType confirmButtonType = new ButtonType("Reset", ButtonBar.ButtonData.OK_DONE);
         dialog.getDialogPane().getButtonTypes().addAll(confirmButtonType, ButtonType.CANCEL);
 
-        // 设置结果转换器，将输入框中的文本返回为结果
         dialog.setResultConverter(dialogButton -> {
             if (dialogButton == confirmButtonType) {
                 return textField.getText();
@@ -49,10 +44,8 @@ public class MainController {
             return null;
         });
 
-        // 显示对话框，并等待用户的输入
         Optional<String> result = dialog.showAndWait();
         result.ifPresent(params -> {
-            // 用户输入参数后，执行git reset命令
             try {
                 executeGitReset(params);
             } catch (IOException e) {
@@ -74,12 +67,10 @@ public class MainController {
     }
 
     private void handleGitRevert() {
-        // 创建一个对话框，用于接收用户输入的参数
         Dialog<String> dialog = new Dialog<>();
         dialog.setTitle("Git Revert");
         dialog.setHeaderText("请输入Git Revert参数：");
 
-        // 设置一个标签和输入框，并将其添加到对话框中
         Label label = new Label("参数：");
         TextField textField = new TextField();
 
@@ -88,11 +79,9 @@ public class MainController {
         layout.setSpacing(10);
         dialog.getDialogPane().setContent(layout);
 
-        // 添加确定和取消按钮
         ButtonType confirmButtonType = new ButtonType("Revert", ButtonBar.ButtonData.OK_DONE);
         dialog.getDialogPane().getButtonTypes().addAll(confirmButtonType, ButtonType.CANCEL);
 
-        // 设置结果转换器，将输入框中的文本返回为结果
         dialog.setResultConverter(dialogButton -> {
             if (dialogButton == confirmButtonType) {
                 return textField.getText();
@@ -100,10 +89,8 @@ public class MainController {
             return null;
         });
 
-        // 显示对话框，并等待用户的输入
         Optional<String> result = dialog.showAndWait();
         result.ifPresent(params -> {
-            // 用户输入参数后，执行git revert命令
             try {
                 executeGitRevert(params);
             } catch (IOException e) {
@@ -113,7 +100,6 @@ public class MainController {
     }
 
     private void executeGitRevert(String params) throws IOException {
-        // 使用参数执行git revert命令，并打印输出
         String command = "git revert " + params;
         Process process = Runtime.getRuntime().exec(command);
 
